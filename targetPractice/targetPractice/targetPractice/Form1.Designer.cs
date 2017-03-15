bb8d835b534032261a27a028bad6c6d6df19b28a
saltttt <xdstringer@gmail.com>
2017-03-15 12:51:32 -0500

    Add files via upload
    v2.0 or v2.5

---
+++ targetPractice/targetPractice/targetPractice/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace targetPractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tGameTime = new System.Windows.Forms.Timer(this.components);
+            this.tBallGenerator = new System.Windows.Forms.Timer(this.components);
+            this.tBallMovement = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new targetPractice.Form1.DoubleBufferedPanel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tGameTime
+            // 
+            this.tGameTime.Tick += new System.EventHandler(this.tGameTime_Tick);
+            // 
+            // tBallGenerator
+            // 
+            this.tBallGenerator.Enabled = true;
+            this.tBallGenerator.Interval = 2000;
+            this.tBallGenerator.Tick += new System.EventHandler(this.tBallGenerator_Tick);
+            // 
+            // tBallMovement
+            // 
+            this.tBallMovement.Enabled = true;
+            this.tBallMovement.Interval = 20;
+            this.tBallMovement.Tick += new System.EventHandler(this.tBallMovement_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.BackgroundImage = global::targetPractice.Properties.Resources.bottom;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.lblTimer);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.panel1.Location = new System.Drawing.Point(2, 677);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(680, 183);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Image = global::targetPractice.Properties.Resources.life3;
+            this.pictureBox3.InitialImage = null;
+            this.pictureBox3.Location = new System.Drawing.Point(239, 88);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(76, 68);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Image = global::targetPractice.Properties.Resources.life3;
+            this.pictureBox2.InitialImage = null;
+            this.pictureBox2.Location = new System.Drawing.Point(140, 88);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(76, 68);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = global::targetPractice.Properties.Resources.life3;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(43, 88);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(76, 68);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.BackColor = System.Drawing.Color.Transparent;
+            this.lblTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer.ForeColor = System.Drawing.Color.White;
+            this.lblTimer.Location = new System.Drawing.Point(474, 99);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(89, 37);
+            this.lblTimer.TabIndex = 4;
+            this.lblTimer.Text = "0000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(684, 861);
+            this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_Closing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Timer tGameTime;
+        public System.Windows.Forms.Timer tBallGenerator;
+        public System.Windows.Forms.Timer tBallMovement;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private DoubleBufferedPanel panel1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lblTimer;
+    }
+}
+
---
+++ targetPractice/targetPractice/targetPractice/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Timers;
+
+namespace targetPractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //this.Click += this.checkIfHit;
+        }
+
+        class DoubleBufferedPanel : Panel { public DoubleBufferedPanel() : base() { DoubleBuffered = true; } }
+
+        int num_Balls = 0;
+        List<Rectangle> ballList = new List<Rectangle>();
+        List<Point> ballVelocity = new List<Point>();
+        Size formSize;
+        Random rand = new Random();
+        System.Timers.Timer t;
+        int h, m, s;
+
+        private void tBallGenerator_Tick(object sender, EventArgs e)
+        {
+            int width = rand.Next(10, 40);
+            ballList.Add(new Rectangle(
+                rand.Next(10, 700 - 3 * width),
+                rand.Next(10, 700 - 3 * width),
+                width, width));
+            int vx = rand.Next(2, 10);
+            int vy = rand.Next(2, 10);
+            if (rand.Next(0, 2) == 0) vx = -vx;
+            if (rand.Next(0, 2) == 0) vy = -vy;
+            ballVelocity.Add(new Point(vx, vy));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Save the form's size.
+            formSize = ClientSize;
+
+            // Use double buffering to reduce flicker.
+            this.SetStyle(
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.UserPaint |
+                ControlStyles.DoubleBuffer,
+                true);
+
+            this.UpdateStyles();
+             
+            t = new System.Timers.Timer();
+            t.Interval = 10;
+            t.Elapsed += onTimeEvent;
+            t.Start();
+        }
+
+        private void tBallMovement_Tick(object sender, EventArgs e)
+        {
+   
+            for (int ball_num = 0; ball_num < ballList.Count; ball_num++)
+            {
+                int old_x = ballVelocity[ball_num].X;
+                int reverse_x = -(ballVelocity[ball_num].X);
+                int old_y = ballVelocity[ball_num].Y;
+                int reverse_y = -(ballVelocity[ball_num].Y);
+
+                // Move the ball.
+                int new_x = ballList[ball_num].X +
+                    ballVelocity[ball_num].X;
+                int new_y = ballList[ball_num].Y +
+                    ballVelocity[ball_num].Y;
+                if (new_x < 0)
+                {
+                    ballVelocity[ball_num] = new Point(-ballVelocity[ball_num].X, ballVelocity[ball_num].Y);
+                }
+                else if (new_x + ballList[ball_num].Width >
+                    formSize.Width)
+                {
+                    ballVelocity[ball_num] = new Point(-ballVelocity[ball_num].X, ballVelocity[ball_num].Y);
+                }
+                if (new_y < 0 + 10)
+                {
+                    ballVelocity[ball_num] = new Point(ballVelocity[ball_num].X, -ballVelocity[ball_num].Y);
+                }
+                else if (new_y + ballList[ball_num].Height >
+                    formSize.Height - 190)
+                {
+                    ballVelocity[ball_num] = new Point(ballVelocity[ball_num].X, -ballVelocity[ball_num].Y);
+                }
+
+                ballList[ball_num] = new Rectangle(
+                    new_x, new_y,
+                    ballList[ball_num].Width,
+                    ballList[ball_num].Height);
+            }
+            this.Refresh();
+        }
+
+        public void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics c = e.Graphics;
+            c.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            c.Clear(BackColor);
+
+            //Bitmap backgroundImage = new Bitmap(targetPractice.Properties.Resources.backgroun2); 
+            //e.Graphics.DrawImage(backgroundImage, this.ClientRectangle,
+       // new Rectangle(0, 0, backgroundImage.Width, backgroundImage.Height),
+        //GraphicsUnit.Pixel);
+          
+            for (int i = 0; i < ballList.Count; i++)
+            {
+                c.DrawImage(targetPractice.Properties.Resources.target1, ballList[i].X, ballList[i].Y, ballList[i].Size.Height, ballList[i].Size.Width);
+            }
+        }
+
+        private void tGameTime_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void onTimeEvent(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                s += 1;
+                if (s == 60)
+                {
+                    s = 0;
+                    m += 1;
+                }
+                if (m == 60)
+                {
+                    m = 0;
+                    h += 1;
+                }
+
+                lblTimer.Text = string.Format("{0}:{1}:{2}", h.ToString().PadLeft(2, '0'), m.ToString().PadLeft(2, '0'), s.ToString().PadLeft(2, '0'));
+            }));
+
+            if (h == 2)
+            {
+                t.Stop();
+                tBallMovement.Stop();
+                tBallGenerator.Stop();
+            }
+        }
+
+        private void Form1_Closing(object sender, FormClosingEventArgs e)
+        {
+            t.Stop();
+            Application.DoEvents();
+        }
+    }
+}
